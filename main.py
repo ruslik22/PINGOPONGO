f5d225d11260197bb0afc21a4a3b4e0cc076701c
dj triple femboy <russiatop2@gmail.com>
2024-03-23 15:19:06 +0300

    create GameSprite class

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ class GameSprite(sprite.Sprite):
     
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+class player(GameSprite):
+    def update_left(self):
+        keys = key_pressed()
+        if keys[K_w] and self.rect.y >0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y + self.rect.height < SCREENSIZE[1]:
+            self.rect.y -= self.speed
+
+    def update_right(self):
+        keys = key_pressed()
+        if keys[K_w] and self.rect.y >0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y + self.rect.height < SCREENSIZE[1]:
+            self.rect.y -= self.speed
+
+
 
 while run:
     for e in event.get():
