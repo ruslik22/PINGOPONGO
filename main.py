917030e4b1cd190154bda61745a841d1f15d573c
dj triple femboy <russiatop2@gmail.com>
2024-03-30 14:27:13 +0300

    lalalal

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ run = True
 class GameSprite(sprite.Sprite):
     def __init__ (self, img, x, y, speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(img), width, height)
-        self.speed = 0
+        self.image = transform.scale(image.load(img),(width, height))
+        self.speed = 4
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -23,19 +23,21 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class player(GameSprite):
     def update_left(self):
-        keys = key_pressed()
-        if keys[K_w] and self.rect.y >0:
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y + self.rect.height < SCREENSIZE[1]:
-            self.rect.y -= self.speed
+            self.rect.y += self.speed
 
     def update_right(self):
-        keys = key_pressed()
-        if keys[K_w] and self.rect.y >0:
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y + self.rect.height < SCREENSIZE[1]:
-            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y + self.rect.height < SCREENSIZE[1]:
+            self.rect.y += self.speed
 
+platform_left = player('platform.png',20,320,4,25,99)
+platform_right = player('platform.png',660,320,4,25,99)
 
 
 while run:
@@ -43,7 +45,21 @@ while run:
         if e.type == QUIT:
             run = False
 
+
     window.fill(BACKCOLOR)
+
+
+
+    platform_left.reset()
+    platform_right.reset()
+
+    platform_left.update_left()
+    platform_right.update_right()
+
+
+    
+
+
     display.update()
     timer.tick(60)
 
